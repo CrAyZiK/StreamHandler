17cc03dd78b500dfddd7a93e7d9fb98cc77abb17
CrAyZiK <41376435+CrAyZiK@users.noreply.github.com>
2022-07-09 21:12:55 +0300

    Update StreamHandler.h

--- StreamHandler/StreamHandler.h
+++ StreamHandler/StreamHandler.h
@@ -8,32 +8,30 @@
 #include <sstream>
 #include <algorithm>
 #include <numeric>
+#include <queue>
 
 namespace StreamHandler 
 {
 	class Processing
 	{
 	public:
-		Processing();
+		Processing(int W);
 		static uint8_t sum_of_parameters(std::vector<uint8_t> parameters);
-		static uint8_t handmade_sum_of_parameters(std::vector<uint8_t> parameters);
 		static uint8_t product_of_parameters(std::vector<uint8_t> parameters);
-		static uint8_t handmade_product_of_parameters(std::vector<uint8_t> parameters);
 		static uint8_t max_of_parameters(std::vector<uint8_t> parameters);
-		static uint8_t handmade_max_of_parameters(std::vector<uint8_t> parameters);
 		static uint8_t min_of_parameters(std::vector<uint8_t> parameters);
-		static uint8_t handmade_min_of_parameters(std::vector<uint8_t> parameters);
 
 		std::vector<uint8_t> compute_blocks(const std::string blocks);
-		static uint8_t median(std::vector<uint8_t> r_stream);
-		static uint8_t handmade_median(std::vector<uint8_t> r_stream);
-
+		uint8_t median();
+		void push(uint8_t element);
+		void push(std::vector<uint8_t> elements);
+		bool queue_is_full();
 	private:
+		int MAX_QUEUE_SIZE;
 
+		uint32_t queue_item_count[255] = {0};
+
+		std::queue<uint8_t> queue;
 		std::map<uint8_t, std::function<uint8_t(std::vector<uint8_t>)>> rules;
-
-		static uint8_t handmade_nth_element(std::vector<uint8_t> vec, int first, int k, int last);
-		static int partition(std::vector<uint8_t> vec, int first, int last);
-
 	};
 }
